265da15fb3236a3a8ddc0f19165bc59fb46ba97a
Maxime HERVÉ <maxime.herve.22@protonmail.com>
2025-02-07 14:48:11 +0100

    updating tests

--- fast-api/tests/test_cves.py
+++ fast-api/tests/test_cves.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
 from main import app
 
