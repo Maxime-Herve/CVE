41bf943faf2997c0440c0b59a7eeded8d04727ea
Maxime HERVÉ <maxime.herve.22@protonmail.com>
2025-02-07 14:37:54 +0100

    Deleting old tests

--- unittest/fast-api/test_cves.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from cves import function_name  # CHANGE ME
-
-class TestCVES(unittest.TestCase):
-    
-    def test_function_name(self): #CHANGE ME
-
-if __name__ == "__main__":
-    unittest.main()
