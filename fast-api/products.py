a48a0af31f6958c81a715466d90824f2e3a98ffd
Maxime HERVÉ <maxime.herve.22@protonmail.com>
2025-02-10 14:59:11 +0100

    Adding some products function

--- fast-api/products.py
+++ fast-api/products.py
@@ -1,7 +1,58 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
+import requests
+from requests.auth import HTTPBasicAuth
 
 router = APIRouter()
 
-@router.get("/product")
-def product_hello():
-    return {"Product": "Hello"}
+username = "maxime.herve@etu.mines-ales.fr"
+password = "T*fFDSUY3@LQ0O"
+
+@router.get("/vendors/{vendor_name}")
+def get_vendor_info(vendor_name: str):
+    url = f"https://app.opencve.io/api/vendors/{vendor_name}"
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+    if response.status_code == 200:
+        return response.json()
+
+    raise HTTPException(status_code=response.status_code, detail=response.json())
+
+@router.get("/vendors/{vendor_name}/cve")
+def get_vendor_cve(vendor_name: str, page: int = Query(None, description="Page number for pagination")):
+    if page:
+        url = f"https://app.opencve.io/api/vendors/{vendor_name}/cve?page={page}"
+    else:
+        url = f"https://app.opencve.io/api/vendors/{vendor_name}/cve" 
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+    if response.status_code == 200:
+        return response.json()
+
+    raise HTTPException(status_code=response.status_code, detail=response.json())
+
+@router.get("/vendors/{vendor_name}/products/{product_name}")
+def get_product_info(vendor_name: str, product_name: str):
+    url = f"https://app.opencve.io/api/vendors/{vendor_name}/products/{product_name}"
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+    if response.status_code == 200:
+        return response.json()
+
+    raise HTTPException(status_code=response.status_code, detail=response.json())
+
+@router.get("/vendors/{vendor_name}/products/{product_name}/cve")
+def get_product_cve(vendor_name: str, product_name: str, page: int = Query(None, description="Page number for pagination")):
+    if page:
+        url = f"https://app.opencve.io/api/vendors/{vendor_name}/products/{product_name}/cve?page={page}"
+    else:
+        url = f"https://app.opencve.io/api/vendors/{vendor_name}/products/{product_name}/cve" 
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+    if response.status_code == 200:
+        return response.json()
+
+    raise HTTPException(status_code=response.status_code, detail=response.json())
